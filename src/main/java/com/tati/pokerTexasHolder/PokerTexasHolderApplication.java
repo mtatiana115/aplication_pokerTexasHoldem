eec5bc5558ff15d5b5b0e14e253acf8fa062ee08
mtatiana115 <mtatiana115@gmail.com>
2024-08-16 10:23:42 -0500

    Base project

---
+++ src/main/java/com/tati/pokerTexasHolder/PokerTexasHolderApplication.java
@@ -0,0 +1,13 @@
+package com.tati.pokerTexasHolder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerTexasHolderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerTexasHolderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tati/pokerTexasHolder/PokerTexasHolderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tati.pokerTexasHolder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerTexasHolderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
